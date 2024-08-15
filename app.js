bcd09ee145857ee75073c1b4d21a3654b957fc17
Gabesz007 <szolgalati.torocsik@gmail.com>
2024-08-15 14:50:54 +0200

    css and app.js continued

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+
 import './styles.css';
 
 console.log('<--> Eddig működik.')
+
+fetch('https://data.police.uk/api/crimes-street-dates')
+    .then(response => {
+    if (!response.ok) {
+        throw new Error('Nincsen válasz, HIBA!' + response.statusText);
+    }
+    return response.json(); 
+    })
+    .then(data => {
+    console.log(data); 
+    })
+    .catch(error => {
+    console.error('Probléma van a fetchel:', error);
+    });
+
 
